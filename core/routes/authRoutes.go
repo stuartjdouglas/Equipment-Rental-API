c0201067d3acbf0eadbca3ce860f9d99eb50594f
remony <sudouglas@dundee.ac.uk>
2015-10-27 14:22:26 +0000

    fixed some issues

--- core/routes/authRoutes.go
+++ core/routes/authRoutes.go
@@ -7,6 +7,7 @@ import(
 	"net"
 	"fmt"
 	"github.com/zenazn/goji/web"
+	"strings"
 )
 
 func generateAuthRoutes(api router.API)	{
@@ -20,24 +21,21 @@ func generateAuthRoutes(api router.API)	{
 
 	api.Router.Post("/login", func (c web.C, res http.ResponseWriter, r *http.Request) {
 		var loginDetails = login{
-			Username:r.FormValue("username"),
+			Username: r.FormValue("username"),
 			Password: r.FormValue("password"),
 		}
-
-
+		
 		if len(loginDetails.Username) == 0 || len(loginDetails.Password) == 0{
-			fmt.Println("Get user info from json");
-
 			err := json.NewDecoder(r.Body).Decode(&loginDetails)
 			if err != nil {
 				http.Error(res, err.Error(), http.StatusBadRequest)
 			}
 		}
+//		TODO Remove IP address recording
 		ip_address, _, _ := net.SplitHostPort(r.RemoteAddr)
-		println("username: " + r.FormValue("username") + "  |  Password: " + r.FormValue("password"))
 
 		var login models.Auth
-		login = models.LoginUser(api, loginDetails.Username, loginDetails.Password, ip_address)
+		login = models.LoginUser(api, strings.ToLower(loginDetails.Username), loginDetails.Password, ip_address)
 
 		if(login.Success) {
 			data, err := json.Marshal(login)
@@ -53,8 +51,5 @@ func generateAuthRoutes(api router.API)	{
 			res.WriteHeader(http.StatusUnauthorized)
 			json.NewEncoder(res).Encode(error_response{Message:"Invalid Username or Password"})
 		}
-
-
-
 	})
 }
