80cd6b4051f2ffcb527862fd34776b2880b5a956
remon <remonasebi@gmail.com>
2015-12-16 16:49:32 +0000

    updated unit test

--- core/models/sessions/sessionsModel_test.go
+++ core/models/sessions/sessionsModel_test.go
@@ -18,7 +18,7 @@ router := router.API{Context: database.Connection(config.LoadConfig(CONF_FILE, t
 g.Describe("Sessions", func() {
 //		TODO get token from a login
 g.It("Should be valid", func() {
-g.Assert(IsSessionValid(router, "a17a095f-3db6-4635-bf5c-071bc26089e6")).IsTrue()
+g.Assert(IsSessionValid(router, "301dfa1a-a2da-462d-b029-26c793782a0f")).IsTrue()
 })
 
 g.It("Should be invalid", func() {
