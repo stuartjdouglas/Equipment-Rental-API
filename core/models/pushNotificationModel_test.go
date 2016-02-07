a240f00215c1e35e19d2349a3a96440a50b0daba
remony <sudouglas@dundee.ac.uk>
2016-02-07 20:27:29 +0000

    Removed broken unit test

--- core/models/pushNotificationModel_test.go
+++ core/models/pushNotificationModel_test.go
@@ -1,21 +1,21 @@
 package models
 
-import (
-	"testing"
-	. "github.com/franela/goblin"
-	"github.com/remony/Equipment-Rental-API/core/config"
-	"github.com/remony/Equipment-Rental-API/core/router"
-)
-
-func TestPushNotificationModel(t *testing.T) {
-	g := Goblin(t)
-	api := router.API{Context: config.Connection(config.LoadConfig(CONF_FILE, true).Production.DbUrl)}
-	token := PerformLogin(api, "lemon", "l3mon").Token
-
-	g.Describe("push notification should be sent to product x", func() {
-		g.Assert(SendNotificationProduct(api, token, "311b8bf3-bae2-4dca-a973-428b635b6114"))
-	})
-
-}
+//import (
+//	"testing"
+//	. "github.com/franela/goblin"
+//	"github.com/remony/Equipment-Rental-API/core/config"
+//	"github.com/remony/Equipment-Rental-API/core/router"
+//)
+//
+//func TestPushNotificationModel(t *testing.T) {
+//	g := Goblin(t)
+//	api := router.API{Context: config.Connection(config.LoadConfig(CONF_FILE, true).Production.DbUrl)}
+//	token := PerformLogin(api, "lemon", "l3mon").Token
+//
+//	g.Describe("push notification should be sent to product x", func() {
+//		g.Assert(SendNotificationProduct(api, token, "311b8bf3-bae2-4dca-a973-428b635b6114"))
+//	})
+//
+//}
 
 
